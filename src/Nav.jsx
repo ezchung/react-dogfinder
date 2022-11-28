ac6d3a4b358c79167f5b63b5cd5b568603ae2995
Ezra Chung <ezra.chung97@gmail.com>
2022-11-28 15:36:09 -0800

    Added css to active links

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { isCompositeComponentWithType } from "react-dom/test-utils";
 import { NavLink } from "react-router-dom";
-
+import "./Nav.css";
 /**
  * 
  * @param {*} param0 
  * @returns 
  */
+
 function Nav({dogs}){
 
     return (
         <nav className="Nav">
             {dogs.map(dog =>
-                <NavLink className="dog" key={dog.name} to={`/dogs/${dog.name}`}>
-                    <h2>{dog.name}</h2>
+                <NavLink 
+                    className={({ isActive }) =>
+                        isActive ? "dog active" : undefined
+                    }
+                    key={dog.name} 
+                    to={`/dogs/${dog.name}`}
+                    >
+                        <h2>{dog.name}</h2>
                 </NavLink>
             )}
         </nav>
